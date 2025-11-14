7617c30115a279a283a4dce57710cc735c24c861
supermanten <supermanten8@gmail.com>
2025-11-14 14:26:06 +0530

    config qtile and nushell

--- configuration.nix
+++ configuration.nix
@@ -1,55 +1,38 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [ ./hardware-configuration.nix ];
 
-  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "Asia/Kolkata";
-  
+
   services.xserver.windowManager.qtile.enable = true;
 
   services.displayManager.ly.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-   users.users.rio = {
-     isNormalUser = true;
-     shell = pkgs.nushell;
-     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-     packages = with pkgs; [
-       tree
-     ];
-   };
+  users.users.rio = {
+    isNormalUser = true;
+    shell = pkgs.nushell;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [ tree ];
+  };
 
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
-   environment.systemPackages = with pkgs; [
-     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-     wget
-     nushell
-   ];
+  environment.systemPackages = with pkgs; [ vim wget nushell ];
 
-   programs.firefox.enable = true;
+  programs.firefox.enable = true;
 
-   fonts.packages = with pkgs;[
-      nerd-fonts.jetbrains-mono 
-  ];
+  fonts.packages = with pkgs; [ nerd-fonts.jetbrains-mono ];
 
-   nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-
-  system.stateVersion = "25.05"; # Did you read the comment?
+  system.stateVersion = "25.05";
 
 }
 
